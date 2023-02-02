c6a34a930039deabee4e5e42ca65eba071fceffc
Rishiraj <53556561+wabrishi@users.noreply.github.com>
2023-02-02 07:40:50 +0000

    nLargest

--- pythonAtoZ/nLargestNumber.py
+++ pythonAtoZ/nLargestNumber.py
@@ -1,15 +1,7 @@
 
-def nLargest():
+def nLargest(n):
     a=[1,572,13,14,55,61,7,123438,2349]
-    l=len(a)
-    n=2
-    n1=0
-    n2=0
-    for i in range(1,l):
-        if(n1<a[i]):
-            n1=a[i]
-        if(n2<a[i] & a[i]<n1):
-            n2=a[i]
-    print(n1,"\n",n2)
+    a.sort(reverse=True)
+    print(a[:n])
 if __name__ == "__main__":
-    nLargest()
+    nLargest(3)
